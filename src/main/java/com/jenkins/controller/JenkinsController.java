fdc1a7bc20ed86b9ab4814947cf1d33b12f38ad1
barbimalendu <bimalendu.bar@maxmobility.biz>
2023-08-09 19:39:51 +0530

    added project

---
+++ src/main/java/com/jenkins/controller/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+	@GetMapping("/display")
+	public String disply() {
+		return "Hi";
+	}
+
+}
